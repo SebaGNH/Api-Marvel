8ea32be9b1017ac82fdf77e4a762b417f385c8c1
Sebastián González <seba.gnh@hgmail.com>
2021-07-04 23:18:01 -0300

    Buscador funcionando

--- script/script.js
+++ script/script.js
@@ -1,15 +1,30 @@
 "use strict"
 const RenderDOM = {
     render: () => {
+        /* Inicio <-- Declaración de variables ------------------------------------*/
         const conexion_api = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=eaa98daf4d86236acb4de698f6808297&hash=c0819d4ad93eb938110b0d68f54532f0';
         const container = document.getElementById('id-Contenedor-superHero');        
         let contenidoHTML = '';
         const noImg = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
 
-        
 
         let idBuscar = document.getElementById('idBuscar');
         idBuscar.value = "";
+
+        const btn_prev = document.getElementById("btn-prev");
+        const btn_next = document.getElementById("btn-next");
+        //btn_prev.disabled = false;
+        //btn_next.disabled = false;
+
+
+        /*-- Fin  <-- Declaración de variables ------------------------------------*/
+        
+        /* Inicio <-- Paginación ------------------------------------*/
+        let pageNumber = 5;
+        let pageSize = 5;
+        let pagination;
+        let pageCont = Math.ceil(conexion_api.length/pageSize);
+        /*-- Fin  <-- Paginación ------------------------------------*/
         
         fetch(conexion_api)
         .then(res => res.json())
@@ -17,8 +32,6 @@ const RenderDOM = {
 
             console.log(json,"RES.JSON");
             personajesCompleto();
-
-
 
             idBuscar.addEventListener('keyup', (e)=> {  
             
@@ -32,15 +45,28 @@ const RenderDOM = {
             });
 
 
+            function paginate(array, page_size, page_number) {
+                return array.slice((page_number - 1) * page_size, page_number * page_size);
+            }
+
+            function nextPage(){
+                pageNumber ++;
+                //showNoticias(pagination)
+            }
+            function previusPage(){
+                pageNumber --;
+                //showNoticias(pagination)
+            }
 
 
             function personajesCompleto(){
+                var pagination = paginate(conexion_api,pageSize,pageNumber);
                 contenidoHTML = "";                
                 for (const superHero of json.data.results) {
                     let urlHero = superHero.urls[1].url;
-                        //if(noImg!== superHero.thumbnail.path){                            
-                            contenidoHTML += `
-                            <div class="llamada">
+                        //if(noImg!== superHero.thumbnail.path){  
+                            contenidoHTML += `<div class="llamada">`;                          
+                            contenidoHTML += `                            
                                 <div class="thumb">
                                     <a href="${urlHero}" target="_blank">
                                         <img src="${superHero.thumbnail.path}.${superHero.thumbnail.extension}" alt="${superHero.name}">
@@ -56,19 +82,27 @@ const RenderDOM = {
                       //  }
                        // console.log(superHero.name);
                     }
-                    return container.innerHTML = contenidoHTML;
+                    
+                    pageNumber >1  ? btn_prev.disabled = false :"";
+                    pageNumber < pageCont ?(btn_next.disabled = false):"" ;
+                    return container.innerHTML = contenidoHTML; 
+                    //return container.innerHTML = contenidoHTML;
             }
 
+
+
+            
+/* Inicio <--  Buscador ------------------------------------*/   
             function SinpersonajesCompleto(nombreBuscado){
-                contenidoHTML = "";
-                
+                contenidoHTML = "";                
 
                 for (const superHero of json.data.results) {
                     let urlHero = superHero.urls[1].url;
                     const nombreAPI = superHero.name.toLowerCase();
                     const nombreRecibido = nombreBuscado.toLowerCase();
-
-                    if(nombreRecibido== nombreAPI){
+                    //console.log(nombreRecibido);
+                    //console.log(nombreAPI);
+                    if(nombreAPI.includes(nombreRecibido)){
                         contenidoHTML += `
                         <div class="llamada">
                             <div class="thumb">
@@ -87,6 +121,8 @@ const RenderDOM = {
                 }
                 container.innerHTML = contenidoHTML;
             }
+/*-- Fin  <--  Buscador ------------------------------------*/
+
         })        
     }
 };
